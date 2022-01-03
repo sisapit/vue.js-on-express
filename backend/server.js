9c6649d31d20a0a13ba4e13c2cae81750180e507
Sebastian Scheffler <seb.scheffler@gmx.de>
2022-01-03 17:25:51 +0100

    Excluded currently not needed module

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8000;
